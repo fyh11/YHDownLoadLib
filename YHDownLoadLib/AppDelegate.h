82618202b0e5c9595e7e7f51efcf72d6e387f686
Your Name <you@example.com>
2017-06-01 14:14:40 +0800

    Initial Commit

---
+++ YHDownLoadLib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHDownLoadLib
+//
+//  Created by 樊义红 on 17/6/1.
+//  Copyright © 2017年 樊义红. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YHDownLoadLibTests/YHDownLoadLibTests.m
@@ -0,0 +1,39 @@
+//
+//  YHDownLoadLibTests.m
+//  YHDownLoadLibTests
+//
+//  Created by 樊义红 on 17/6/1.
+//  Copyright © 2017年 樊义红. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YHDownLoadLibTests : XCTestCase
+
+@end
+
+@implementation YHDownLoadLibTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
